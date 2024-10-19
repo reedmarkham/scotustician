5eb6a9f266418f902eff62e9a11a360c6eca4f7d
Reed Markham <reedmarkham@gmail.com>
2024-10-19 00:27:39 -0400

    completed test script for FastAPI

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json, os
 
 from fastapi import FastAPI
 from ratelimit import limits, sleep_and_retry
-import requests, boto3
+import requests, httpx, boto3
 
 DESCRIPTION= '''
 A FastAPI wrapper to interact with the Oyez.org API for Supreme Court case data
@@ -11,8 +11,8 @@ A FastAPI wrapper to interact with the Oyez.org API for Supreme Court case data
 # Oyez API URLs:
 OYEZ_CASE_SUMMARY = 'https://api.oyez.org/cases?per_page=0'
 OYEZ_CASES_TERM_PREFIX = 'https://api.oyez.org/cases?per_page=0&filter=term:'
-def oyez_api_case(term: int, case_id: str):
-    return f'https://api.oyez.org/cases/{term}/{case_id}'
+def oyez_api_case(term: int, docket_number: str):
+    return f'https://api.oyez.org/cases/{term}/{docket_number}'
 
 # S3 URIs:
 S3_CASE_SUMMARY = os.environ['S3_CASE_SUMMARY']
@@ -28,29 +28,38 @@ app = FastAPI(
 
 @sleep_and_retry
 @limits(calls=1, period=1)
-def request(url):
+def request(url: str):
     try:
         return requests.get(url).json()
     except:
         print(f'API response: {requests.get(url).status_code}')
 
-@app.get("/case_summary/")
-def case_summary():
-    return request(OYEZ_CASE_SUMMARY)
+@app.get("/case_summary")
+async def case_summary():
+    async with httpx.AsyncClient() as client:
+        response = await client.get(OYEZ_CASE_SUMMARY)
+        case_summary = response.json()
+        return case_summary
 
-@app.post("/sync_case_summary/")
+@app.post("/sync_case_summary")
 def sync_case_summary():
     s3 = boto3.client('s3')
     s3.put_object(
-        Body=json.dumps(case_summary()),
+        Body=json.dumps(request(OYEZ_CASE_SUMMARY)),
         Bucket=S3_CASE_SUMMARY,
         Key=CASE_SUMMARY_KEY
     )
 
 @app.get("/cases_by_term/{term}")
-def cases_by_term (term: int):
-    return request(OYEZ_CASES_TERM_PREFIX+term)
+async def cases_by_term(term: int):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(OYEZ_CASES_TERM_PREFIX+str(term))
+        cases = response.json()
+        return cases
 
-@app.get("/case_full/")
-def case_full (term: int, case_id: str):
-    return request(oyez_api_case(term, case_id))
+@app.get("/case_full/{term}/{docket_number}")
+async def case_full(term: int, docket_number: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(oyez_api_case(term, docket_number))
+        case_full = response.json()
+        return case_full
--- test.py
+++ test.py
@@ -11,39 +11,42 @@ s3 = boto3.client('s3')
 print('Intialized S3 ...')
 
 # Print out case summary
-print(requests.get('http://127.0.0.1:8000/get/case_summary').json())
+# print(requests.get('http://127.0.0.1:8000/case_summary').json())
 
 # Load case summaries to S3
-requests.post('http://127.0.0.1:8000/post/sync_case_summary')
+requests.post('http://127.0.0.1:8000/sync_case_summary')
 print('Synced case summary to S3 ...')
 
 # Load all case fulls to S3
-case_summaries = requests.get('http://127.0.0.1:8000/get/case_summary').json()
-print(case_summaries)
-for case in case_summaries:
-    print(case)
-    key = f'case_full_{case.ID}.json'
+case_summaries = requests.get('http://127.0.0.1:8000/case_summary').json()
+
+for case in case_summaries[0:1]:
+    case_id = case['ID']
+    key = f'case_full_{case_id}.json'
+    
+    case_href = case['href']
     s3.put_object(
-        Body = json.dumps(requests.get(case.href).json()),
+        Body = json.dumps(requests.get(case_href).json()),
         Bucket = CASE_FULL_BUCKET,
         Key = key
     )
     print(f'Loaded: s3://{CASE_FULL_BUCKET}/{key} ...')
 
 # Specify terms of interest and iterate through cases
-terms = [1965, 1972]
+terms = [2020, 2022]
 for term in terms:
-    cases = requests.get(f'http://127.0.0.1:8000/get/cases_by_term/{term}')[0:1]
-    print(cases)
+    cases = requests.get(f'http://127.0.0.1:8000/cases_by_term/{term}').json()
     for case in cases[0:1]:
-        case_full = requests.get(f'http://127.0.0.1:8000/case_full/?term={term}&case={case_id}')
-        print(case_full)
+        docket_number = case['docket_number']
+        case_full = requests.get(f'http://127.0.0.1:8000/case_full/{term}/{docket_number}').json()
         # Get oral argument data and load to S3
         if ('oral_argument_audio' in case_full and case_full['oral_argument_audio']):
-            for oa in case['oral_argument_audio']:
-                key = f'oa_{oa.id}.json'
+            for oa in case_full['oral_argument_audio']:
+                oa_id = oa['id']
+                key = f'oa_{oa_id}.json'
+                oa_href = oa['href']
                 s3.put_object(
-                    Body = json.dumps(requests.get(oa.href).json()),
+                    Body = json.dumps(requests.get(oa_href).json()),
                     Bucket = OA_BUCKET,
                     Key = key
                 )
