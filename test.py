4a0846c7975fa35211963082d1d455cdd307135f
Reed Markham <reedmarkham@gmail.com>
2024-10-19 01:02:37 -0400

    refining test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import json
+import json, pprint
 
 import requests, boto3
 
@@ -17,9 +17,8 @@ print('Intialized S3 ...')
 requests.post('http://127.0.0.1:8000/sync_case_summary')
 print('Synced case summary to S3 ...')
 
-# Load all case fulls to S3
+# Load case fulls to S3
 case_summaries = requests.get('http://127.0.0.1:8000/case_summary').json()
-
 for case in case_summaries[0:1]:
     case_id = case['ID']
     key = f'case_full_{case_id}.json'
@@ -32,20 +31,22 @@ for case in case_summaries[0:1]:
     )
     print(f'Loaded: s3://{CASE_FULL_BUCKET}/{key} ...')
 
-# Specify terms of interest and iterate through cases
+# Specify terms of interest, and iterate through cases; later, load some oral arguments to S3
 terms = [2020, 2022]
 for term in terms:
     cases = requests.get(f'http://127.0.0.1:8000/cases_by_term/{term}').json()
     for case in cases[0:1]:
         docket_number = case['docket_number']
         case_full = requests.get(f'http://127.0.0.1:8000/case_full/{term}/{docket_number}').json()
-        # Get oral argument data and load to S3
         if ('oral_argument_audio' in case_full and case_full['oral_argument_audio']):
             for oa in case_full['oral_argument_audio']:
                 oa_id = oa['id']
                 key = f'oa_{oa_id}.json'
                 oa_href = oa['href']
-                print(requests.get(oa_href).json()['transcript']['sections'][0]['turns'][0]) # Sample of transcript
+
+                print('-'*20,'Sample of Supreme Court oral argument: ', '-'*20)
+                pprint.pprint(requests.get(oa_href).json()['transcript']['sections'][0]['turns'][0], compact=True) 
+
                 s3.put_object(
                     Body = json.dumps(requests.get(oa_href).json()),
                     Bucket = OA_BUCKET,
