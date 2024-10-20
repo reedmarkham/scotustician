f923a807efa2777a8bc505a4aea2822d99398078
reedmarkham <reedmarkham@gmail.com>
2024-10-19 22:14:58 -0400

    Update s3_to_csv.py

--- dev/s3_to_csv.py
+++ dev/s3_to_csv.py
@@ -3,7 +3,8 @@ import json
 import pandas as pd, boto3
 
 S3_CASE_SUMMARY = 'scotustician-case-summary
-CASE_SUMMARY_KEY = 'case-summary.json
+CASE_SUMMARY_KEY = 'case-summary.json'
+CASE_SUMMARY_CSV = './case-summary.csv'
 
 s3 = boto3.resource('s3')
 
@@ -12,3 +13,5 @@ case_summary = s3.Object(S3_CASE_SUMMARY, CASE_SUMMARY_KEY)
 case_summary_df = pd.DataFrame.from_records(
     json.loads(case_summary.get()['Body'].read().decode('utf-8'))
     )
+
+case_summary_df.to_csv(CASE_SUMMARY_CSV)
